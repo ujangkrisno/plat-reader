6cc71debfe4bb21c611c32864200abd6fbe2504c
ujangkrisno <ujang.krisno@gmail.com>
2026-05-24 07:09:57 +0700

    fix setup.php drop tables first to avoid 'already exists'

--- setup.php
+++ setup.php
@@ -1,5 +1,8 @@
 <?php
 include 'config/database.php';
+// Drop existing tables
+foreach (['plat_nomor','cameras'] as $t) @mysqli_query($con, "DROP TABLE IF EXISTS $t");
+// Create from SQL
 $sql = file_get_contents(__DIR__ . '/database.sql');
 foreach (explode(';', $sql) as $q) {
     $q = trim($q);
