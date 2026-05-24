56a3e19d5fec99931f4fd74b1741a8006e86f8d2
ujangkrisno <ujang.krisno@gmail.com>
2026-05-24 07:07:27 +0700

    fix database.php error handling + setup path

--- config/database.php
+++ config/database.php
@@ -3,10 +3,13 @@ $host = '127.0.0.1';
 $user = 'root';
 $pass = '';
 $db = 'plat_reader';
-$con = mysqli_connect($host, $user, $pass, $db);
+mysqli_report(MYSQLI_REPORT_OFF);
+$con = @mysqli_connect($host, $user, $pass, $db);
 if (!$con) {
-    // Try creating database
     $con = mysqli_connect($host, $user, $pass);
-    mysqli_query($con, "CREATE DATABASE IF NOT EXISTS $db");
-    mysqli_select_db($con, $db);
+    if ($con) {
+        mysqli_query($con, "CREATE DATABASE IF NOT EXISTS $db");
+        mysqli_select_db($con, $db);
+    }
 }
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
--- setup.php
+++ setup.php
@@ -1,6 +1,6 @@
 <?php
 include 'config/database.php';
-$sql = file_get_contents('database.sql');
+$sql = file_get_contents(__DIR__ . '/database.sql');
 foreach (explode(';', $sql) as $q) {
     $q = trim($q);
     if ($q) mysqli_query($con, $q);
